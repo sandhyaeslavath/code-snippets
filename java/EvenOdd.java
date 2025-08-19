67d3a8b91bb8a123a1364dc877f4228a1c31a8f9
sandhyaeslavath <esandhya_cse230582@mgit.ac.in>
2025-08-19 14:25:47 +0530

    Create EvenOdd.java

---
+++ java/EvenOdd.java
@@ -0,0 +1,23 @@
+// EvenOdd.java
+// Program to check whether a number is even or odd
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input number
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        // Check even or odd
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+
+        sc.close();
+    }
+}
